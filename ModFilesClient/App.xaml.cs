fa87c7b98971da237c4ec58165847d02a7dc278b
ToasterGod <adh01@hotmail.se>
2020-10-24 16:00:44 +0200

    Added a WPF interface which the program will use from now on.

---
+++ ModFilesClient/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModFilesClient.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ModFilesClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost host;
+        public static IServiceProvider ServiceProvider { get; private set; }
+        public App()
+        {
+            host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) => ConfigureAppServices(context,services))
+                .Build();
+            ServiceProvider = host.Services;
+        }
+
+        private void ConfigureAppServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddSingleton<MainViewModel>();
+            services.AddTransient<MainWindow>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await host.StartAsync();
+            MainWindow mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (host)
+            {
+                await host.StopAsync();
+            }
+            base.OnExit(e);
+        }
+    }
+}
---
+++ ModFilesClient/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace ModFilesClient.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public MainViewModel MainViewModel
+        {
+            get
+            {
+                return GetMainViewModel().Result;
+            }
+        }
+
+        public static Task<MainViewModel> GetMainViewModel()
+        {
+            MainViewModel vm = App.ServiceProvider.GetRequiredService<MainViewModel>();
+
+            return Task.FromResult(vm);
+        }
+    }
+}
